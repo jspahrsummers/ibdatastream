285eefbf740559c35ab4ca904e0406b5470e0263
Justin Spahr-Summers <justin@jspahrsummers.com>
2019-09-08 15:19:55 +0100

    Stub a simple test

---
+++ tests/test_server.py
@@ -0,0 +1,18 @@
+import unittest
+
+import grpc
+import concurrent.futures as futures
+from ibdatastream import ibdatastream_pb2_grpc
+from ibdatastream import server
+
+
+class TestServer(unittest.TestCase):
+    def test_start_server(self) -> None:
+        s = grpc.server(futures.ThreadPoolExecutor())
+        ibdatastream_pb2_grpc.add_IBDataStreamServicer_to_server(server.Servicer(), s)
+        s.add_insecure_port("[::]:50051")
+        s.start()
+
+
+if __name__ == "__main__":
+    unittest.main()
