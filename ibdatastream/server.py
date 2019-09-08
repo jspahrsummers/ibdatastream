ca05dd849170405bbfc61fd8f1e143171282a7eb
Justin Spahr-Summers <justin@jspahrsummers.com>
2019-09-08 15:43:05 +0100

    Really basic test for LookUp request

--- ibdatastream/server.py
+++ ibdatastream/server.py
@@ -1,9 +1,9 @@
-from . import ibdatastream_pb2_grpc
+from . import ibdatastream_pb2, ibdatastream_pb2_grpc
 
 
 class Servicer(ibdatastream_pb2_grpc.IBDataStreamServicer):
     def LookUp(self, request, context):
-        pass
+        return ibdatastream_pb2.Contract(contractID=0)
 
     def Subscribe(self, request, context):
         pass
--- tests/test_server.py
+++ tests/test_server.py
@@ -1,17 +1,27 @@
+import concurrent.futures as futures
 import unittest
 
 import grpc  # type: ignore
-import concurrent.futures as futures
-from ibdatastream import ibdatastream_pb2_grpc
-from ibdatastream import server
+
+from ibdatastream import ibdatastream_pb2, ibdatastream_pb2_grpc, server
 
 
 class TestServer(unittest.TestCase):
-    def test_start_server(self) -> None:
+    port = 50051
+    stub: ibdatastream_pb2_grpc.IBDataStreamStub
+
+    def setUp(self) -> None:
         s = grpc.server(futures.ThreadPoolExecutor())
         ibdatastream_pb2_grpc.add_IBDataStreamServicer_to_server(server.Servicer(), s)
-        s.add_insecure_port("[::]:50051")
+        s.add_insecure_port(f"[::]:{self.port}")
         s.start()
+
+        channel = grpc.insecure_channel(f"localhost:{self.port}")
+        self.stub = ibdatastream_pb2_grpc.IBDataStreamStub(channel)
+
+    def test_look_up(self) -> None:
+        contract = self.stub.LookUp(ibdatastream_pb2.ContractLookup(symbol="SPY"))
+        self.assertIsNotNone(contract)
 
 
 if __name__ == "__main__":
